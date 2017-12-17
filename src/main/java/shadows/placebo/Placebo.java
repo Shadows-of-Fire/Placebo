c4874a19c9574cf8a7838b697a650ce163a4b85f
Shadows_of_Fire <Bward7864@gmail.com>
2017-12-16 21:43:19 -0500

    initial commit
    might do somethin soon, not rit now

---
+++ src/main/java/shadows/placebo/Placebo.java
@@ -0,0 +1,14 @@
+package shadows.placebo;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Placebo.MODID, name = Placebo.MODNAME, version = Placebo.VERSION)
+public class Placebo {
+
+	public static final String MODID = "placebo";
+	public static final String MODNAME = "Placebo";
+	public static final String VERSION = "1.0.0";
+	
+	
+	//holy shit it doesn't do anything but lets see who checks that out
+}
