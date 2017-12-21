f43ad992584490f0b2db83f0165da44cbf954e1a
Shadows_of_Fire <Bward7864@gmail.com>
2017-12-21 18:14:03 -0500

    blockable collections

---
+++ src/main/java/shadows/placebo/util/collections/BlockedDeque.java
@@ -0,0 +1,40 @@
+package shadows.placebo.util.collections;
+
+import java.util.ArrayDeque;
+
+public abstract class BlockedDeque<T> extends ArrayDeque<T> {
+
+	private static final long serialVersionUID = -8194197029368437188L;
+
+	public abstract boolean isBlocked(T t);
+	
+	@Override
+	public void addFirst(T t) {
+		if (isBlocked(t)) return;
+		super.addFirst(t);
+	}
+
+	@Override
+	public void addLast(T t) {
+		if (isBlocked(t)) return;
+		super.addLast(t);
+	}
+
+	@Override
+	public boolean add(T t) {
+		addLast(t);
+		return !isBlocked(t);
+	}
+
+	@Override
+	public boolean offerFirst(T t) {
+		addFirst(t);
+		return !isBlocked(t);
+	}
+
+	@Override
+	public boolean offerLast(T t) {
+		addLast(t);
+		return !isBlocked(t);
+	}
+}
---
+++ src/main/java/shadows/placebo/util/collections/BlockedList.java
@@ -0,0 +1,37 @@
+package shadows.placebo.util.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class BlockedList<T> extends ArrayList<T> {
+
+	private static final long serialVersionUID = -7008599240020028661L;
+
+	public abstract boolean isBlocked(T t);
+	
+	public abstract boolean isBlocked(Collection<? extends T> c);
+	
+	@Override
+	public void add(int index, T t) {
+		if(isBlocked(t)) return;
+		super.add(index, t);
+	}
+	
+	@Override
+	public boolean add(T t) {
+		if(isBlocked(t)) return false;
+		return super.add(t);
+	}
+	
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		if(isBlocked(c)) return false;
+		return super.addAll(c);
+	}
+	
+	@Override
+	public boolean addAll(int index, Collection<? extends T> c) {
+		if(isBlocked(c)) return false;
+		return super.addAll(index, c);
+	}
+}
