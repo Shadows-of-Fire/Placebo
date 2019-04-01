9f62f38ab638bbdc42dfa74fdc90d5bfe937ec49
Shadows_of_Fire <Bward7864@gmail.com>
2019-04-01 13:40:36 -0400

    Better data loading model

--- src/main/java/shadows/placebo/patreon/PatreonManager.java
+++ src/main/java/shadows/placebo/patreon/PatreonManager.java
@@ -8,7 +8,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
+import java.util.function.Consumer;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.MathHelper;
@@ -19,10 +22,21 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import shadows.placebo.Placebo;
 
+/**
+ * Patreon Data Loader.  Loads from a file on the Placebo github.
+ * Data uses a plaintext format, and the only rules to the format are that the first entry must be a number, then a space.
+ * @author Shadows
+ *
+ */
 @EventBusSubscriber(modid = Placebo.MODID, value = Side.CLIENT)
 public class PatreonManager {
 
+	public static final Int2ObjectMap<Consumer<String>> DATA_PARSERS = new Int2ObjectOpenHashMap<>();
 	public static final Map<UUID, EnumParticleTypes> PATREONS = new HashMap<>();
+
+	static {
+		DATA_PARSERS.put(0, PatreonManager::parseV0Data);
+	}
 
 	@SubscribeEvent
 	public static void init(ModelRegistryEvent e) {
@@ -33,14 +47,18 @@ public class PatreonManager {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 				while (reader.ready()) {
 					String s = reader.readLine();
-					String[] split = s.split(" ");
+					String[] split = s.split(" ", 2);
 					if (split.length != 2) {
 						Placebo.LOG.error("Invalid patreon data entry {} will be ignored.", s);
 						continue;
 					}
-					UUID id = UUID.fromString(split[0]);
-					EnumParticleTypes type = EnumParticleTypes.getByName(split[1]);
-					PATREONS.put(id, type);
+					int dataVer = Integer.parseInt(split[0]);
+					Consumer<String> func = DATA_PARSERS.get(dataVer);
+					if (func == null) {
+						Placebo.LOG.error("Invalid patreon data entry {} with invalid data version {} will be ignored.", s, dataVer);
+						continue;
+					}
+					func.accept(split[1]);
 				}
 				reader.close();
 			} catch (IOException ex) {
@@ -50,13 +68,24 @@ public class PatreonManager {
 		}, "Placebo Patreon Loader").start();
 	}
 
+	static void parseV0Data(String s) {
+		String[] split = s.split(" ");
+		if (split.length != 2) {
+			Placebo.LOG.error("Invalid patreon data entry (version 0) {} will be ignored.", s);
+			return;
+		}
+		UUID id = UUID.fromString(split[0]);
+		EnumParticleTypes type = EnumParticleTypes.getByName(split[1]);
+		PATREONS.put(id, type);
+	}
+
 	@SubscribeEvent
 	public static void onPlayerTick(PlayerTickEvent e) {
 		if (e.player.world.isRemote) {
-			EnumParticleTypes type = PATREONS.get(e.player.getGameProfile().getId());
-			if (type == null) return;
 			Random rand = Minecraft.getMinecraft().world.rand;
 			if (rand.nextInt(35) == 0) {
+				EnumParticleTypes type = PATREONS.get(e.player.getGameProfile().getId());
+				if (type == null) return;
 				double x = e.player.prevPosX;
 				double y = e.player.prevPosY;
 				double z = e.player.prevPosZ;
