eee4923430dfc08f273273b0496602f299136f43
Brennan Ward <Bward7864@gmail.com>
2019-09-27 17:28:10 -0400

    shift util method

---
+++ src/main/java/shadows/placebo/util/ClientUtil.java
@@ -0,0 +1,14 @@
+package shadows.placebo.util;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.InputMappings;
+
+public class ClientUtil {
+
+	public static boolean isHoldingShift() {
+		return (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT) || InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT));
+	}
+
+}
