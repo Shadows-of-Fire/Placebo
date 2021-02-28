7a34dbc43d74dd392c77c8ea7cd3cfb6b0f00938
Shadows_of_Fire <Bward7864@gmail.com>
2021-02-28 16:25:43 -0500

    Fix the ItemUseEvent having to recall vanilla code

--- src/main/java/shadows/placebo/events/PlaceboEventHooks.java
+++ src/main/java/shadows/placebo/events/PlaceboEventHooks.java
@@ -1,17 +1,10 @@
 package shadows.placebo.events;
 
-import java.util.function.Function;
-
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.CachedBlockInfo;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 
 public class PlaceboEventHooks {
@@ -24,28 +17,7 @@ public class PlaceboEventHooks {
 		ItemUseEvent event = new ItemUseEvent(ctx);
 		MinecraftForge.EVENT_BUS.post(event);
 		if (event.isCanceled()) return event.getCancellationResult();
-		if (!ctx.getWorld().isRemote) return net.minecraftforge.common.ForgeHooks.onPlaceItemIntoWorld(ctx);
-		return onItemUse(stack, ctx, c -> stack.getItem().onItemUse(ctx));
-	}
-
-	/**
-	 * Vanilla (Patch) Copy :: {@link ItemStack#onItemUse(ItemUseContext, Function)}
-	 */
-	public static ActionResultType onItemUse(ItemStack stack, ItemUseContext ctx, Function<ItemUseContext, ActionResultType> callback) {
-		PlayerEntity playerentity = ctx.getPlayer();
-		BlockPos blockpos = ctx.getPos();
-		CachedBlockInfo cachedblockinfo = new CachedBlockInfo(ctx.getWorld(), blockpos, false);
-		if (playerentity != null && !playerentity.abilities.allowEdit && !stack.canPlaceOn(ctx.getWorld().getTags(), cachedblockinfo)) {
-			return ActionResultType.PASS;
-		} else {
-			Item item = stack.getItem();
-			ActionResultType actionresulttype = callback.apply(ctx);
-			if (playerentity != null && actionresulttype == ActionResultType.SUCCESS) {
-				playerentity.addStat(Stats.ITEM_USED.get(item));
-			}
-
-			return actionresulttype;
-		}
+		return null;
 	}
 
 	/**
--- src/main/resources/coremods/item_use_hook.js
+++ src/main/resources/coremods/item_use_hook.js
@@ -13,16 +13,23 @@ function initializeCoreMod() {
                 var AbstractInsnNode = Java.type('org.objectweb.asm.tree.AbstractInsnNode');
                 var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
                 var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
+                var LabelNode = Java.type('org.objectweb.asm.tree.LabelNode');
+                var JumpInsnNode = Java.type('org.objectweb.asm.tree.JumpInsnNode');
                 var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
                 var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
                 var instr = method.instructions;
-				ASMAPI.log('INFO', 'Patching ItemStack#onItemUse');
+                ASMAPI.log('INFO', 'Patching ItemStack#onItemUse');
 
                 var insn = new InsnList();
+                var label = new LabelNode();
                 insn.add(new VarInsnNode(Opcodes.ALOAD, 0));
                 insn.add(new VarInsnNode(Opcodes.ALOAD, 1));
                 insn.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "shadows/placebo/events/PlaceboEventHooks", "onItemUse", "(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemUseContext;)Lnet/minecraft/util/ActionResultType;", false));
+                insn.add(new InsnNode(Opcodes.DUP));
+                insn.add(new JumpInsnNode(Opcodes.IFNULL, label));
                 insn.add(new InsnNode(Opcodes.ARETURN));
+                insn.add(label);
+                insn.add(new InsnNode(Opcodes.POP));
                 instr.insert(insn);
 
                 return method;
