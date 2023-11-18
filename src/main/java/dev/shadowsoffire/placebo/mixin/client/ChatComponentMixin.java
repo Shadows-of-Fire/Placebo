37342771cd4d003bf15cbe7c924e56b10ac0d98f
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-11-17 18:04:46 -0800

    Fix ChatComponentMixin not being in the client package

--- src/main/java/dev/shadowsoffire/placebo/mixin/client/ChatComponentMixin.java
+++ src/main/java/dev/shadowsoffire/placebo/mixin/client/ChatComponentMixin.java
@@ -1,4 +1,4 @@
-package dev.shadowsoffire.placebo.mixin;
+package dev.shadowsoffire.placebo.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
