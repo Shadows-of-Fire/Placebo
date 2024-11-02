5359b678064be4d3228aeae03300f1e4a24cda2a
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-11-02 15:17:36 -0700

    Re-add GearSetRegistry#getRandomSet

--- src/main/java/dev/shadowsoffire/placebo/systems/gear/GearSetRegistry.java
+++ src/main/java/dev/shadowsoffire/placebo/systems/gear/GearSetRegistry.java
@@ -1,7 +1,18 @@
 package dev.shadowsoffire.placebo.systems.gear;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.jetbrains.annotations.Nullable;
+
 import dev.shadowsoffire.placebo.Placebo;
 import dev.shadowsoffire.placebo.reload.WeightedDynamicRegistry;
+import dev.shadowsoffire.placebo.systems.gear.GearSet.SetPredicate;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.random.WeightedEntry.Wrapper;
+import net.minecraft.util.random.WeightedRandom;
 
 public class GearSetRegistry extends WeightedDynamicRegistry<GearSet> {
 
@@ -9,6 +20,27 @@ public class GearSetRegistry extends WeightedDynamicRegistry<GearSet> {
 
     public GearSetRegistry() {
         super(Placebo.LOGGER, "gear_sets", false, false);
+    }
+
+    /**
+     * Returns a random weighted armor set based on the given random (and predicate, if applicable).
+     */
+    @Nullable
+    public GearSet getRandomSet(RandomSource rand, float luck, @Nullable List<SetPredicate> armorSets) {
+        if (armorSets == null || armorSets.isEmpty()) return this.getRandomItem(rand, luck);
+        List<GearSet> valid = this.registry.values().stream().filter(e -> {
+            for (Predicate<GearSet> f : armorSets)
+                if (f.test(e)) return true;
+            return false;
+        }).collect(Collectors.toList());
+        if (valid.isEmpty()) {
+            Placebo.LOGGER.error("Failed to locate any gear sets matching the following predicates: ");
+            armorSets.forEach(s -> Placebo.LOGGER.error(s.toString()));
+            return this.getRandomItem(rand, luck);
+        }
+        List<Wrapper<GearSet>> list = new ArrayList<>(valid.size());
+        valid.stream().map(l -> l.<GearSet>wrap(luck)).forEach(list::add);
+        return WeightedRandom.getRandomItem(rand, list).map(Wrapper::data).orElse(null);
     }
 
     @Override
