ae05beb7a85cbf2bb1fb94de449778d2250cfd81
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-11-13 09:53:47 -0800

    Add support for bounds-style StepFunction(s)

--- src/main/java/dev/shadowsoffire/placebo/util/StepFunction.java
+++ src/main/java/dev/shadowsoffire/placebo/util/StepFunction.java
@@ -1,7 +1,5 @@
 package dev.shadowsoffire.placebo.util;
 
-import java.util.function.Function;
-
 import com.google.common.base.Preconditions;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
@@ -13,17 +11,32 @@ import net.minecraft.network.FriendlyByteBuf;
 /**
  * Interpolator that allows for only returning "nice" stepped numbers.
  */
-public record StepFunction(float min, int steps, float step) implements Float2FloatFunction {
+public record StepFunction(float min, int steps, float step, float max) implements Float2FloatFunction {
 
     /**
      * Accepts a fully defined step function with min, steps, and step values.
+     * 
+     * @deprecated Prefer {@link #BOUNDS_CODEC}.
      */
+    @Deprecated
     public static final Codec<StepFunction> STRICT_CODEC = RecordCodecBuilder.create(inst -> inst
         .group(
             Codec.FLOAT.fieldOf("min").forGetter(StepFunction::min),
             Codec.intRange(1, Integer.MAX_VALUE).fieldOf("steps").forGetter(StepFunction::steps),
             Codec.FLOAT.fieldOf("step").forGetter(StepFunction::step))
         .apply(inst, StepFunction::new));
+
+    /**
+     * Accepts a bounds-style StepFunction, which defines a min / max and the step value.
+     */
+    public static final Codec<StepFunction> BOUNDS_CODEC = RecordCodecBuilder.create(inst -> inst
+        .group(
+            Codec.FLOAT.fieldOf("min").forGetter(StepFunction::min),
+            Codec.FLOAT.fieldOf("max").forGetter(StepFunction::max),
+            Codec.FLOAT.optionalFieldOf("step", 0.01F).forGetter(StepFunction::step))
+        .apply(inst, StepFunction::fromBounds));
+
+    public static final Codec<StepFunction> TRANSITION_CODEC = Codec.either(STRICT_CODEC, BOUNDS_CODEC).xmap(Either::unwrap, Either::right);
 
     /**
      * Accepts a single float value that will produce a {@linkplain StepFunction#constant constant step function}.
@@ -33,7 +46,12 @@ public record StepFunction(float min, int steps, float step) implements Float2Fl
     /**
      * Either codec between {@link STRICT_CODEC} and {@link CONSTANT_CODEC}, producing a step function from either a single float or the full definition.
      */
-    public static final Codec<StepFunction> CODEC = Codec.either(CONSTANT_CODEC, STRICT_CODEC).xmap(e -> e.map(Function.identity(), Function.identity()), StepFunction::toEither);
+    public static final Codec<StepFunction> CODEC = Codec.either(CONSTANT_CODEC, TRANSITION_CODEC).xmap(Either::unwrap, StepFunction::toEither);
+
+    @Deprecated
+    public StepFunction(float min, int steps, float step) {
+        this(min, steps, step, min + steps * step);
+    }
 
     /**
      * Create a new StepFunction
@@ -42,11 +60,13 @@ public record StepFunction(float min, int steps, float step) implements Float2Fl
      * @param steps The max number of steps
      * @param step  The value per step
      */
-    public StepFunction(float min, int steps, float step) {
+    public StepFunction(float min, int steps, float step, float max) {
         this.min = min;
         this.steps = steps;
         this.step = step;
-        Preconditions.checkArgument(steps > 0);
+        this.max = max;
+        Preconditions.checkArgument(steps > 0, "Steps must be a positive integer");
+        Preconditions.checkArgument(Math.abs((min + steps * step) - max) <= 0.00001F, "Max value is out-of-sync with other fields.");
     }
 
     @Override
@@ -56,10 +76,6 @@ public record StepFunction(float min, int steps, float step) implements Float2Fl
 
     public int getInt(float level) {
         return (int) this.get(level);
-    }
-
-    public float max() {
-        return this.min + this.steps * this.step;
     }
 
     /**
@@ -94,10 +110,11 @@ public record StepFunction(float min, int steps, float step) implements Float2Fl
 
     public static StepFunction fromBounds(float min, float max, float step) {
         int steps = Math.round((max - min) / step);
-        StepFunction function = new StepFunction(min, steps, step);
-        if (Math.abs(function.max() - max) > 0.001F) {
-            throw new UnsupportedOperationException("Failed to interpolate step function bounds");
+        if (Math.abs((min + step * steps) - max) > 0.0001F) {
+            throw new UnsupportedOperationException("Failed to interpolate step function bounds with min=" + min + "; max=" + max + "; step=" + step
+                + ". The step value must be a multiple of the difference between min and max.");
         }
+        StepFunction function = new StepFunction(min, steps, step, max);
         return function;
     }
 
