7261d2a4aec5c2b9cbf0612102f925ace68d6703
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-01-04 12:03:36 -0800

    Extend DrawsOnLeft to support lists of FormattedText

--- src/main/java/dev/shadowsoffire/placebo/util/DrawsOnLeft.java
+++ src/main/java/dev/shadowsoffire/placebo/util/DrawsOnLeft.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.ItemStack;
 
 /**
@@ -19,8 +20,10 @@ public interface DrawsOnLeft {
 
     /**
      * Renders a list of text as a tooltip attached to the left edge of the currently open container screen.
+     * <p>
+     * This method will automatically compress the text to fit in the available space between the left edge of the screen and the left edge of the game window.
      */
-    default void drawOnLeft(GuiGraphics gfx, List<Component> list, int y) {
+    default void drawOnLeft(GuiGraphics gfx, List<? extends FormattedText> list, int y) {
         if (list.isEmpty()) return;
         int xPos = __ths().getGuiLeft() - 16 - list.stream().map(__ths().font::width).max(Integer::compare).get();
         int maxWidth = 9999;
@@ -30,22 +33,30 @@ public interface DrawsOnLeft {
         }
 
         List<FormattedText> split = new ArrayList<>();
-        int lambdastupid = maxWidth;
-        list.forEach(comp -> split.addAll(__ths().font.getSplitter().splitLines(comp, lambdastupid, comp.getStyle())));
+        int _maxWidth = maxWidth;
+        list.forEach(text -> {
+            Style style = text instanceof Component comp ? comp.getStyle() : Style.EMPTY;
+            __ths().font.getSplitter().splitLines(text, _maxWidth, style, (splitLine, isBlank) -> split.add(splitLine));
+        });
 
         gfx.renderComponentTooltip(__ths().font, split, xPos, y, ItemStack.EMPTY);
     }
 
     /**
      * Renders a list of text as a tooltip attached to the left edge of the currently open container screen.
+     * <p>
+     * This method will compress the text to fit in the specified maxWidth, ignoring the size of the game window.
      */
-    default void drawOnLeft(GuiGraphics gfx, List<Component> list, int y, int maxWidth) {
+    default void drawOnLeft(GuiGraphics gfx, List<? extends FormattedText> list, int y, int maxWidth) {
         if (list.isEmpty()) {
             return;
         }
 
         List<FormattedText> split = new ArrayList<>();
-        list.forEach(comp -> split.addAll(__ths().font.getSplitter().splitLines(comp, maxWidth, comp.getStyle())));
+        list.forEach(text -> {
+            Style style = text instanceof Component comp ? comp.getStyle() : Style.EMPTY;
+            __ths().font.getSplitter().splitLines(text, maxWidth, style, (splitLine, isBlank) -> split.add(splitLine));
+        });
 
         int xPos = __ths().getGuiLeft() - 16 - split.stream().map(__ths().font::width).max(Integer::compare).get();
         gfx.renderComponentTooltip(__ths().font, split, xPos, y, ItemStack.EMPTY);
