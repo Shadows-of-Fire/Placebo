2c723081344e7260709605a5adfd49a10e52a47f
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2026-04-21 02:46:35 -0400

    Restore StackLootEntry

--- src/main/java/dev/shadowsoffire/placebo/loot/StackLootEntry.java
+++ src/main/java/dev/shadowsoffire/placebo/loot/StackLootEntry.java
@@ -7,9 +7,9 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import dev.shadowsoffire.placebo.json.OptionalStackCodec;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
@@ -25,38 +25,39 @@ public class StackLootEntry extends LootPoolSingletonContainer {
 
     public static final MapCodec<StackLootEntry> CODEC = RecordCodecBuilder.mapCodec(inst -> inst
         .group(
-            OptionalStackCodec.INSTANCE.fieldOf("stack").forGetter(e -> e.stack),
-            Codec.intRange(0, 64).fieldOf("min").forGetter(e -> e.min),
-            Codec.intRange(0, 64).fieldOf("max").forGetter(e -> e.max))
+            ItemStackTemplate.CODEC.fieldOf("stack").forGetter(e -> e.stack),
+            Codec.intRange(0, 99).fieldOf("min").forGetter(e -> e.min),
+            Codec.intRange(0, 99).fieldOf("max").forGetter(e -> e.max))
         .and(singletonFields(inst))
         .apply(inst, StackLootEntry::new));
 
-    private final ItemStack stack;
+    private final ItemStackTemplate stack;
     private final int min;
     private final int max;
 
-    public StackLootEntry(ItemStack stack, int min, int max, int weight, int quality, List<LootItemCondition> conditions, List<LootItemFunction> functions) {
+    public StackLootEntry(ItemStackTemplate stack, int min, int max, int weight, int quality, List<LootItemCondition> conditions, List<LootItemFunction> functions) {
         super(weight, quality, conditions, functions);
         this.stack = stack;
         this.min = min;
         this.max = max;
     }
 
-    public StackLootEntry(ItemStack stack, int min, int max, int weight, int quality) {
+    public StackLootEntry(ItemStackTemplate stack, int min, int max, int weight, int quality) {
         this(stack, min, max, weight, quality, List.of(), List.of());
     }
 
+    @SuppressWarnings("deprecation")
     public StackLootEntry(ItemLike item, int min, int max, int weight, int quality) {
-        this(new ItemStack(item), min, max, weight, quality);
+        this(new ItemStackTemplate(item.asItem().builtInRegistryHolder()), min, max, weight, quality);
     }
 
-    public StackLootEntry(ItemStack stack) {
+    public StackLootEntry(ItemStackTemplate stack) {
         this(stack, 1, 1, 1, 0);
     }
 
     @Override
     protected void createItemStack(Consumer<ItemStack> list, LootContext ctx) {
-        ItemStack s = this.stack.copy();
+        ItemStack s = this.stack.create();
         s.setCount(Mth.nextInt(ctx.getRandom(), this.min, this.max));
         list.accept(s);
     }
